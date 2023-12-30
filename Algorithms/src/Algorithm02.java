9459883468cadcfa1a828864f6e4ee28aff54008
Burak AKGÖZ <111371809+Brkakgz@users.noreply.github.com>
2023-12-30 14:55:03 +0300

    Update Algorithm02.java

--- Algorithms/src/Algorithm02.java
+++ Algorithms/src/Algorithm02.java
@@ -13,12 +13,14 @@ public class Algorithm02 {
         num2= reader.nextInt();
         System.out.println("Girilen birinci sayı:" +num2);
 
-        //if(num1==0 || num2==0) num1==0 veya num2==0 anlamına gelir tek seferde yazılır daha rahat
+        //if(num1==0 || num2==0) num1==0 veya num2==0 anlamına gelir tek seferde yazılır daha rahat kolay olabilir tek if içerisinde iki sorgu yapılmış olur5
         //{
         //    System.out.println("Maalesef 0 girdiniz. Uygulama Kapatılıyor");
         //    System.exit( status :-1);
         //}
 
+
+        //Buradaki if yapılarını sayıları girdikten sonra yaparsak ilk girilen sayı0 olursa gerisini işlemez ve hızlı olur
         if(num1==0 )
         {
             System.out.println("Maalesef 0 girdiniz. Uygulama Kapatılıyor");
