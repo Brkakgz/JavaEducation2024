dea38d2b6ecaf90239dc98648d1024f2451c28e7
Burak AKGÖZ <111371809+Brkakgz@users.noreply.github.com>
2024-01-07 14:44:49 +0300

    Update dowhileexample

--- Loops/src/DoWhileExample/doWhileExample.java
+++ Loops/src/DoWhileExample/doWhileExample.java
@@ -3,7 +3,7 @@ package DoWhileExample;
 public class doWhileExample {
     public static void main(String[] args) {
         int start=0;
-        // Do While yapılırken önce sorgusuz yap sonra sorgula
+        // Do While yapılırken önce sorgusuz yap sonra sorgula. Yani önce do içini yapar sonra while ile kontrol yapar eğer kontrol doğru ile do içerisine geri döner
         do
         {
             System.out.println("Start Valut:"+start);
