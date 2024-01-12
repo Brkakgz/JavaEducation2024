a3e50044b05ccdeb0565304be7383f2d96def8f4
Burak AKGÖZ <111371809+Brkakgz@users.noreply.github.com>
2024-01-12 18:07:24 +0300

    Update Question8.java

--- JavaEducationHomeWork-08.01.2024/src/Question8.java
+++ JavaEducationHomeWork-08.01.2024/src/Question8.java
@@ -4,12 +4,15 @@ public class Question8 {
     public static void main(String[] args) {
         //Kullanıcıdan alınan bir sayının asal çarpanlarını bulan bir do-while döngüsü içeren bir Java programı nasıl yazılır?
         Scanner scanner = new Scanner(System.in);
-        int num1;
+        int num1,i=0;
+        System.out.println("Sayınızı giriniz: ");
         num1 = scanner.nextInt();
+        if(num1<=0)
+            return;
+
         do {
 
-
         }
-        while(true);
+        while(i<num1);
     }
 }
