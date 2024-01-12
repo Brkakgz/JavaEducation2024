7c01dce83e95c440c65244ee86a903ca357130a9
Burak AKGÖZ <111371809+Brkakgz@users.noreply.github.com>
2024-01-12 17:23:27 +0300

    Update Question3.java
    Without for loop

--- JavaEducationHomeWork-08.01.2024/src/Question3.java
+++ JavaEducationHomeWork-08.01.2024/src/Question3.java
@@ -1,7 +1,7 @@
 public class Question3 {
     public static void main(String[] args) {
         //Do-while döngüsü kullanarak, 5'e bölünebilen sayıları 1'den 50'ye kadar olan aralıkta ekrana yazdıran bir Java programı
-        int i=1;
+        /*int i=1;
         do {
             for(;i<=50;i++)
             {
@@ -11,6 +11,20 @@ public class Question3 {
                 }
             }
         }
-        while(i<=50 && i>1);
+        while(i<=50 && i>1);*/
+
+        //For Eklemeden yapmayı deneyelim. Aslında burada i sayımızı 1 arttırarak yukarıdaki for döngüsünün işini yapmış olduk
+
+        int i=1;
+        do
+        {
+            if(i%5==0)
+            {
+                System.out.println(i+" sayısı 5'e tam bölünür.");
+            }
+            i++;
+        }
+        while(i<=50);
+        System.out.println("Kapatılıyor...");
     }
 }
