248da48d56754be755a5f4bc7dd6775bcfea9065
Burak AKGÖZ <burak.akgoz.273@gmail.com>
2024-02-03 01:53:29 +0300

    HomeWork

---
+++ JavaHomeWork/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Klavye fiyatlarını giriniz: ");
+        int budget = 60;
+        int [] keyboards = {40,50,60};
+        int [] drives ={5,8,12};
+        int secim=getMoneySpent(budget,keyboards,drives);
+        System.out.println("Bütçenize göre maksimum alabileceğiniz ürünlerin fiyatı: "+secim);
+
+
+
+    }
+    public static int getMoneySpent(int budget,int[] keyboards,int[] drives)
+    {
+        int selectedItem=0,sonuc=-1;
+        for (int i = 0; i < keyboards.length; i++)
+        {
+            for(int j = 0; j < drives.length; j++)
+            {
+                selectedItem=keyboards[i]+drives[j];
+                if(selectedItem<=budget)
+                {
+                sonuc=selectedItem;
+                }
+            }
+        }
+        return sonuc;
+    }
+}
