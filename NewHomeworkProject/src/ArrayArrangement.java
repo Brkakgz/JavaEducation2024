bb0c7b204f770ddf925d7e41eaefca405a44a05a
Burak AKGÖZ <burak.akgoz.273@gmail.com>
2024-03-01 23:57:24 +0300

    Update ArrayArrangement.java
    Update

--- NewHomeworkProject/src/ArrayArrangement.java
+++ NewHomeworkProject/src/ArrayArrangement.java
@@ -5,14 +5,14 @@ public class ArrayArrangement {
         * 1, 2, 3, 10, 6, 4, 7 böyle bi array olsun daha uzun veya kısa olabilir,bunu küçükten büyüğe, Arrays.sort gbi methoflar KULLANMADAN. küçükten büyüğe sıralama yapınız
         * hiç bir java methodu kullanmadan döngüler ile algoritma olarak sırala
         * */
-        int [] array = {1,2,3,10,6,4,7,8,9,9,10,10,11};
+        int [] array = {1,2,3,10,6,4,7,8,9,9,10,10,11,2,5};
         int sayi1=0,sayi2=0,sayi3=0;
 
         for (int i=0;i<array.length;i++)
         {
             for(int ii=0;ii<array.length;ii++)
             {
-              if (array[ii]<=array[i])
+              if (array[i]<=array[ii])
               {
                   sayi1=array[ii];
                   sayi2=array[i];
