67c5cf8a330f60b0ad03ed18202ee137d3a5e0d4
Burak AKGÖZ <burak.akgoz.273@gmail.com>
2024-03-15 11:03:48 +0300

    Homework

--- JavaHomeWorks/JavaHackerRank/JavaHashSet.java
+++ JavaHomeWorks/JavaHackerRank/JavaHashSet.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+//https://www.hackerrank.com/challenges/java-hashset/problem?isFullScreen=true
+
 public class JavaHashSet {
     public static void main(String[] args) {
         Set<String> names= new HashSet<>();
@@ -11,7 +13,7 @@ public class JavaHashSet {
         System.out.print("Kaç adet isim girmek istiyorsunuz: ");
         int nameNumber= scanner.nextInt();
         for(int i=0;i<nameNumber;i++) {
-            String userNames = scanner.nextLine();
+            String userNames = scanner.next(); //nextline yaptığım zaman direkt birinci elemanı boşluk olarak alıyor. next yaparsam da isimde boşluk bırakırsam hata veriyor
             names.add(userNames);
             System.out.println(names.size());
         }
---
+++ JavaHomeWorks/JavaHackerRank/JavaList.java
@@ -0,0 +1,33 @@
+package JavaHackerRank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kaç Adet Sayı Girmek İstiyorsunuz : ");
+        int numberUser = scanner.nextInt();
+        for (int i=0;i<numberUser;i++)
+        {
+            System.out.println("Sayı giriniz: ");
+            int numberChoice = scanner.nextInt();
+            numbers.add(numberChoice);
+        }
+
+        System.out.println("Eklemek istediğiniz sayı: ");
+        int numberUser2= scanner.nextInt();
+        System.out.println("Hangi indexe eklemek istersiniz: ");
+        int addIndexNumber = scanner.nextInt();
+
+        numbers.add(addIndexNumber,numberUser2);
+
+        System.out.println(numbers);
+
+        System.out.println("Hangi indexteki sayıyı silmek istersiniz: ");
+        int removeIndexNumber= scanner.nextInt();
+        numbers.remove(removeIndexNumber);
+        System.out.println(numbers);
+    }
+}
